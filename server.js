2058c3bc28a77800156ba822e7262fe199cea060
Andrey Zimin <zimin6077@gmail.com>
2017-10-29 15:33:33 +0300

    cookies is complete

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const koa = require('koa')
-const logger = require('koa-logger')
 
 const app = new koa()
 
-function testFunc (data) {
-  return async function (ctx, next) {
-    if (/(\.js)$/.test(ctx.path)) {
-      console.log('test')
-      await next()
-    } else {
-      await data.call(this, ctx, next)
-    }
-  }
-}
-
-app.use(testFunc(logger()))
-
 app.use(async (ctx) => {
-  ctx.body = 'Hello word!'
+  const n = ~~ctx.cookies.get('view') + 1
+  ctx.cookies.set('view', n)
+  ctx.body = 'view = ' + n
 })
 
 if (!module.parent) app.listen(5000)
