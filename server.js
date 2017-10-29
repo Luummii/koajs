c4634858f193788f20edf03b5e62eabace286af9
Andrey Zimin <zimin6077@gmail.com>
2017-10-29 16:04:32 +0300

    csrf is complete

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const koa = require('koa')
+const koaBody = require('koa-body')
+const session = require('koa-session')
+const router = require('koa-router')()
+const CSRF = require('koa-csrf')
 
 const app = new koa()
 
-app.use(async (ctx) => {
-  const n = ~~ctx.cookies.get('view') + 1
-  ctx.cookies.set('view', n)
-  ctx.body = 'view = ' + n
-})
+app.keys = ['session key', 'csrf key']
+
+app.use(session(app))
+app.use(koaBody())
+app.use(new CSRF())
+
+router.get('/token', token)
+
+app.use(router.routes())
+
+async function token (ctx) {
+  ctx.body = ctx.csrf
+}
 
 if (!module.parent) app.listen(5000)
