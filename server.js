da22d68d35d89020cc467b12b528566ab0425ad5
Andrey Zimin <zimin6077@gmail.com>
2017-10-29 15:00:40 +0300

    compose is complete

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const koa = require('koa')
-const auth = require('koa-basic-auth')
+const compose = require('koa-compose')
 const app = new koa()
 
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    if (err.status === 401) {
-      ctx.status = 401
-      ctx.set('WWW-Authentificate', 'Basic')
-      ctx.body = 'Jopa'
-    } else {
-      throw err
-    }
-  }
-})
+async function name_1 (ctx, next) {
+  await next()
+  console.log('mes 1')
+}
 
-app.use(auth({ name: 'test', pass: 'test' }))
+async function name_2 (ctx, next) {
+  await next()
+  console.log('mes 2')
+}
 
-app.use(async (ctx) => {
-  ctx.body = 'Secret'
-})
+async function name_3 (ctx, next) {
+  await next()
+  console.log('mes 3')
+}
+
+const all = compose([
+  name_1, name_2, name_3
+])
+
+app.use(all)
 
 if (!module.parent) app.listen(5000)
