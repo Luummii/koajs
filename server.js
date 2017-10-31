7853054bfefd6c675554207698a10b50f3240851
Andrey Zimin <zimin6077@gmail.com>
2017-10-31 15:21:34 +0300

    Post is complete

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const koa = require('koa')
-const app = new koa()
+const render = require('./lib/render')
+const logger = require('koa-logger')
+const router = require('koa-router')()
+const koaBody = require('koa-body')
 
-const Napoleon = {
-  name: 'Napoleon'
+const Koa = require('koa')
+const app = new Koa()
+
+const posts = []
+
+app.use(logger())
+
+app.use(render)
+
+app.use(koaBody())
+
+router.get('/', list)
+      .get('/post/new', add)
+      .get('/post/:id', show)
+      .post('/post', create)
+
+app.use(router.routes())
+
+async function list (ctx) {
+  await ctx.render('list', { posts: posts })
 }
-const Adolf = {
-  name: 'Adolf'
-}
-const Stalin = {
-  name: 'Stalin'
+
+async function add (ctx) {
+  await ctx.render('new')
 }
 
-const users = {
-  Napoleon: Napoleon,
-  Adolf: Adolf,
-  Stalin: Stalin
+async function show (ctx) {
+  const id = ctx.params.id
+  const post = posts[id]
+  if (!post) ctx.throw(404, 'invalid post id')
+  await ctx.render('show', { post: post })
 }
 
-app.use(async (ctx, next) => {
-  await next()
-
-  if (!ctx.body) {
-    console.log('ctx.body', ctx.body)
-    return
-  }
-
-  const type = ctx.accepts('html')
-
-  if (type === false) ctx.throw(406)
-
-  if (type === 'html') {
-    ctx.type = 'html'
-    ctx.body = ctx.body.name
-    return
-  }
-})
-
-app.use(async (ctx, next) => {
-  const name = ctx.path.slice(1)
-  const user = users[name]
-  console.log('user', user)
-  ctx.body = user
-})
-
+async function create (ctx) {
+  const post = ctx.request.body
+  const id = posts.push(post) - 1
+  post.created_at = new Date()
+  post.id = id
+  ctx.redirect('/')
+}
 
 if (!module.parent) app.listen(5000)
