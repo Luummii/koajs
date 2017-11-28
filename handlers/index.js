2e10a30c72d932e01782e74a43d3e424258d7d83
Andrey Zimin (luummii) <luummii@vuejs.fun>
2017-11-28 19:32:39 +0300

    Part 3

---
+++ handlers/index.js
@@ -0,0 +1,14 @@
+import bodyParser from './bodyParser'
+import errors from './errors'
+import favicon from './favicon'
+import logger from './logger'
+import session from './session'
+import static from './static'
+
+export default [
+  favicon,
+  errors,
+  bodyParser,
+  session,
+  static
+]
